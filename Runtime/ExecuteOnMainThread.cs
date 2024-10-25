26b72b6e335b17fc7f72685d61f6b87c2e5a6169
Giorgi Tsaava <giotsa@mail.com>
2024-10-25 16:04:38 +0400

    added ExecuteOnMainThread

---
+++ Runtime/ExecuteOnMainThread.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using UnityEngine;
+
+namespace QuantumServerClient
+{
+
+    public class ExecuteOnMainThread : MonoBehaviour
+    {
+
+        public static readonly ConcurrentQueue<Action> RunOnMainThread = new ConcurrentQueue<Action>();
+
+        void Update()
+        {
+            if (!RunOnMainThread.IsEmpty)
+            {
+                while (RunOnMainThread.TryDequeue(out var action))
+                {
+                    action?.Invoke();
+                }
+            }
+        }
+    }
+}
